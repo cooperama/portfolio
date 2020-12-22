26571b30173cb43f4615b55f84874edcf6b1b8e1
cooperama <coope133@gmail.com>
2020-12-21 20:22:13 -0800

    creates submain branch and updates about me section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../assets/CooperJia2020.pdf";
+import resume from "../assets/AmandaCooper_Resume.pdf";
 
 export const About = (props) => {
   return (
@@ -9,6 +9,14 @@ export const About = (props) => {
       </div>
       <div className="personal-blurb">
         <p>
+          Result-oriented, dedicated, and proactive software developer, with
+          extensive overseas working experience as an educator in two foreign
+          countries. Effective communication skills combined with patience,
+          empathy, and tact allow for fluid collaboration and fast resolution of
+          tasks and problems. Utilizes grit, compassion, and a growth-mindset to
+          develop software that is impactful, meaningful, and functional.
+        </p>
+        {/* <p>
           Full stack developer with a background in ESL education over five
           years and in two foreign countries.
         </p>
@@ -23,7 +31,7 @@ export const About = (props) => {
           It is my objective to create professional, engaging, and fluid
           applications that inspire the user and communicate the product through
           seamless function and thoughtful form.
-        </p>
+        </p> */}
         <p>
           <a className="resume" href={resume} rel="noreferrer" target="_blank">
             My Resume
@@ -34,28 +42,34 @@ export const About = (props) => {
         <h3>Hard Skills</h3>
         <div>
           <ul>
-            <li>HTML/CSS</li>
+            <li>HTML5/CSS3</li>
             <li>JavaScript</li>
             <li>NodeJS</li>
             <li>Express</li>
             <li>ReactJS</li>
+            <li>Redux</li>
             <li>Python</li>
             <li>Django</li>
           </ul>
           <ul>
-            <li>MongoDB</li>
             <li>Mongoose</li>
+            <li>MongoDB</li>
             <li>SQL</li>
             <li>PostgreSQL</li>
             <li>CRUD</li>
+            <li>MVC</li>
             <li>RESTful Routing</li>
+            <li>APIs</li>
           </ul>
           <ul>
+            <li>Git</li>
+            <li>GitHub</li>
             <li>jQuery</li>
             <li>Bootstrap</li>
             <li>Materialize</li>
             <li>SASS</li>
             <li>APIs</li>
+            <li>OOP</li>
           </ul>
         </div>
       </div>
@@ -63,11 +77,25 @@ export const About = (props) => {
         <h3>Soft Skills</h3>
         <div>
           <ul>
-            <li>Teamwork</li>
+            <li>Result-oriented</li>
+            <li>Analytical</li>
             <li>Leadership</li>
             <li>Detail-oriented</li>
+            <li>Innovative</li>
+          </ul>
+          <ul>
+            <li>Cooperative</li>
+            <li>Collaborative</li>
+            <li>Resourceful</li>
             <li>Dependable</li>
-            <li>Growth-mindset</li>
+            <li>Responsible</li>
+          </ul>
+          <ul>
+            <li>Flexible</li>
+            <li>Patient</li>
+            <li>Empathetic</li>
+            <li>Compassionate</li>
+            <li>Growth-oriented</li>
           </ul>
         </div>
       </div>
